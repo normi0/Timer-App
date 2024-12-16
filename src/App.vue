8a698bdf6d4f270245c24f1d2792e1733f718892
normi0 <seif23achi@gmail.com>
2024-12-16 23:01:21 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <section>
+    <header>
+      <div class="wrapper">
+        <HelloWorld msg="You did it!" />
+      </div>
+    </header>
+
+    <main>
+      <TheWelcome />
+    </main>
+  </section>
+</template>
